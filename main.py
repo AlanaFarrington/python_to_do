a6a8026ceac2c6e8be66d101eb6d08093f875ee8
alana_farrington@hotmail.co.uk <alana_farrington@hotmail.co.uk>
2023-08-15 22:06:35 +0100

    added pseudocode for required steps

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-# to do list
-to_do_list = ["Go shopping"]
+# print multistring list of actions that can be done within to do list
+
+# create new item on to do list
+
+# read an iten from to do list
+
+# update item on to do list
+
+# delete item from to do list
+
+# print entire to do list
+to_do_list = {"Go shopping": ["milk", "eggs", "bread"]}
 
 def print_list():
     print(to_do_list)
 
 print_list()
+
+# exit to do list
+def exit_to_do_list():
+    print("""Exiting...
+          To Do List Finished.""")
+    
+# set up loop for continually printing content manager until exit function is executed
